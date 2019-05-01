a78e4f7885d32515bf689b71a05ea742e2c413f6
greeshma0601 <40259116+greeshma0601@users.noreply.github.com>
2019-05-01 15:59:02 +0530

    Create Comment in Python.cpp

---
+++ Comment in Python.cpp
@@ -0,0 +1,45 @@
+'''
+Comment in Python
+You have learnt about input and output in Python. One of the pivotal thing in the world of programming is comment which is used by developers to describe their code or to preclude compiler from compiling the line(s) which they don't want to. In this section, we will learn how to comment any line in Python.
+
+Given two print statement, the task is to comment the first one, leave next as it is and comment next three (multiple line comment) such that only required output will be generated.
+
+Hint:
+#: is used to comment a single line.
+''' ''' : is used to comment multiple lines.
+
+Input:
+None
+
+Output:
+Geeks For Geeks
+
+Example:
+After commenting the required line, Geeks For Geeks is printed, which is required output.
+
+ 
+'''
+//Position this line where user code will be pasted.
+#Python code to use comment
+# Driver Code
+def main():
+    
+    print_func()
+        
+if __name__ == '__main__':
+    main()
+}
+
+''' Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above. '''
+
+#User function Template for python3
+def print_func():
+  #  print ("Geeks Classes")
+    print ("Geeks For Geeks")
+''' 
+print ("Geeks Quiz")
+print ("Geeks")
+print ("Geeks For")
+'''
